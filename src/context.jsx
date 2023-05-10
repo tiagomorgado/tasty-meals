66be95909a118978f69c8bccdb23adaa71ca22e7
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-11 01:32:22 +0200

    Setup of custom hook for context API

--- src/context.jsx
+++ src/context.jsx
@@ -1,12 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 const AppContext = React.createContext()
 
 /* Context API to prevent Prop drilling */
 const AppProvider = ({children}) => {
+    useEffect(() => {
+        console.log('Fetch Data Here')
+    },[])
     return <AppContext.Provider value='hello'>
         {children}
     </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
 }
 
 export {AppContext, AppProvider}
