8e5935d926d44f6a76536e84488b6156a97aec69
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-11 01:10:12 +0200

    small update

--- src/context.jsx
+++ src/context.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 
 const AppContext = React.createContext()
 
+/* Context API to prevent Prop drilling */
 const AppProvider = ({children}) => {
     return <AppContext.Provider value='hello'>
         {children}
