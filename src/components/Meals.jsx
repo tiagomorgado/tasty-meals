9379d68fc8a1991c923cca6caa6bb32ef5d099a0
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-13 19:40:18 +0200

    Added loading info on Fetch

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,9 +2,17 @@ import { useGlobalContext } from "../context"
 import {BsHandThumbsUp} from 'react-icons/bs'
 
 const Meals = () => {
-    const {meals} = useGlobalContext();
+    const {meals, loading} = useGlobalContext();
 
-    return (
+    if(loading) {
+        return (
+            <section className="section">
+                <h4>Loading...</h4>
+            </section>
+        )
+    }
+
+       return (
     <section className='section-center'>
         {
             meals.map((singleMeal) => {
@@ -20,7 +28,7 @@ const Meals = () => {
             })
         }
     </section>
-    )
+    ) 
 }
 
 export default Meals
